3571d35850ea866624704fc3b5a1c023d25bc4ca
juanvazcres <juanvazcres@gmail.com>
2015-06-14 22:08:04 -0500

    archivos que van a hacer que funcione la galería

---
+++ application/models/Item_modelo.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Item_modelo extends CI_Model {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('');
+    }
+
+    public function insert_item($data){
+        $this->db->insert("Item",array('idItem'=>$data['idItem'],));
+    }
+
+    public function get_items_by_type($type){
+        $this->db->where('TipoItem_idTipoItem',$type);
+        $query = $this->db->get('Item');
+        if($query->num_rows()>0){return $query;}
+        else{return false;}
+    }
+
+    public funtion get_items_by_title($title){
+
+        $this->db->where('TipoItem_idTipoItem',$type);
+        $query = $this->db->get('Item');
+        if($query->num_rows()>0){return $query;}
+        else{return false;}
+    }
+
+}
+?>
