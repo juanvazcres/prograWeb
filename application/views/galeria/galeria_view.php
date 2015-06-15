e9b2ffca7ea3854638f426b53c49b18682ef8fdb
juanvazcres <juanvazcres@gmail.com>
2015-06-14 23:01:06 -0500

    galeria_view con lista de videos XD

--- application/views/galeria/galeria_view.php
+++ application/views/galeria/galeria_view.php
@@ -4,24 +4,21 @@
       <div class="inside">
         <div class="wrapper">
 
-
             <!--seccion de galería-->
             <?php foreach($all_videos->result() as $video):?>
             <div class="row">
                 <div class="col-md-3">
                     <a href="#" class="thumbnail">
-                    <img src="" alt="...">
+                    <img src="<?php echo $video->imagen?>" alt="...">
                     </a>
                 </div>
                 <div class="col-md-9">
                     <a href="#" class="thumbnail">
                     <h2>
-                        Titulo de la imagen
+                        <?php echo $video->titulo?>
                     </h2>
                     </a>
-                    <p>Texto descriptivo de la imafgen a refenrenciar.....
-                    kajshdkjashdkjashdkjashd akshd kajsdh kjahd kajshd kjashd kasjhd kasdhask
-                    jdhaskjdh askjdhsa kjdh sakjdh askjhd askjd.</p>
+                    <p><?php echo $video->descripcion?></p>
                 </div>
             </div>
             <?php endforeach; ?>
